646728674749528857214c84efca1a84446cfc53
Naixes <615411375@qq.com>
2020-09-15 16:08:05 +0800

    进度条功能

--- miniprogram/components/progress-bar/progress-bar.js
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -1,7 +1,16 @@
 // components/progress-bar/progress-bar.js
 const backgroundAudioManager = wx.getBackgroundAudioManager()
+// 可动区域宽度
 let moveableAreaWidth = 0
+// 滑块宽度
 let moveableViewWidth = 0
+// 当前秒数
+let currentSec = 0
+// 总时长
+let duration = 0
+// 当前是否移动，优化进度条拖动效果，锁
+// 在小程序中应该尽量减少setData的频率
+let isMoving = false
 Component({
   /**
    * 组件的属性列表
@@ -37,6 +46,28 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onChange(e) {
+      if(e.detail.source === 'touch') {
+        console.log(e)
+        // 暂存拖动的值
+        this.data.progress = e.detail.x / (moveableAreaWidth - moveableViewWidth) * 100
+        this.data.moveableDis = e.detail.x
+        isMoving = true
+      }
+    },
+    // 注意：onTouchEnd执行完成之后有一定概率还会执行onChange，isMoving会出错，所以在onPlay中也设置了isMoving
+    onTouchEnd() {
+      console.log('onTaouchEnd')
+      console.log(backgroundAudioManager.currentTime)
+      // 设置拖动的值
+      this.setData({
+        progress: this.data.progress,
+        moveableDis: this.data.moveableDis
+      })
+      // 设置播放
+      backgroundAudioManager.seek(duration * this.data.progress / 100)
+      isMoving = false
+    },
     // 获取滑动宽度
     _getMoveableDis() {
       const query = this.createSelectorQuery()
@@ -52,6 +83,7 @@ Component({
     _bindAudioEvent() {
       backgroundAudioManager.onPlay(() => {
         console.log('onPlay')
+        isMoving = false
       })
 
       backgroundAudioManager.onStop(() => {
@@ -80,11 +112,27 @@ Component({
 
       // 音乐进入后台播放时不会触发
       backgroundAudioManager.onTimeUpdate(() => {
-        console.log('onTimeUpdate')
+        // console.log('onTimeUpdate')
+        if(!isMoving) {
+          const currentTime = backgroundAudioManager.currentTime
+          // 优化：1s触发一次
+          const sec = currentTime.toString().split('.')[0]
+          if(sec !== currentSec) {
+            const currentTimeFmt = this._formatDuration(currentTime)
+            this.setData({
+              moveableDis: (moveableAreaWidth - moveableViewWidth) * currentTime / duration,
+              progress: currentTime / duration * 100,
+              ['showTime.currentTime']: `${currentTimeFmt.min}:${currentTimeFmt.sec}`
+            })
+            // console.log(sec)
+            currentSec = sec
+          }
+        }
       })
 
       backgroundAudioManager.onEnded(() => {
         console.log("onEnded")
+        this.triggerEvent('playEnd')
       })
 
       backgroundAudioManager.onError((res) => {
@@ -96,7 +144,7 @@ Component({
       })
     },
     _setTime() {
-      const duration = backgroundAudioManager.duration
+      duration = backgroundAudioManager.duration
       const durationFmt = this._formatDuration(duration)
       this.setData({
         ['showTime.totalTime']: `${durationFmt.min}:${durationFmt.sec}`
